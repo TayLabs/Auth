b396d182f960c77c0839cfe068004a376a539bdd
Taylor Kelley <taylorgkelley@outlook.com>
2025-11-17 16:39:55 -0600

    fix: moved password validation to sign up schema and trimed login back down

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,25 +1,8 @@
-import env from '@/types/env';
 import z from 'zod';
-
-const passwordValidation = z
-  .string('Password must be a valid string')
-  .min(8, 'Password must be at least 8 characters long')
-  .max(31, 'Password must be at most 31 characters long');
-
-if (env.CHECK_PASSWORD_COMPLEXITY) {
-  passwordValidation
-    .regex(
-      /[!@#$%^&*(),.?":{}|<>]/,
-      'Password must contain at least one special character'
-    )
-    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .regex(/[0-9]/, 'Password must contain at least one number');
-}
 
 const loginBodySchema = z.object({
   email: z.email('Invalid email address'),
-  password: passwordValidation,
+  password: z.string('Password must be a valid string'),
 });
 
 type LoginBody = z.infer<typeof loginBodySchema>;
---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,35 @@
+import env from '@/types/env';
+import z from 'zod';
+
+const passwordValidation = z
+  .string('Password must be a valid string')
+  .min(8, 'Password must be at least 8 characters long')
+  .max(31, 'Password must be at most 31 characters long');
+
+if (env.CHECK_PASSWORD_COMPLEXITY) {
+  passwordValidation
+    .regex(
+      /[!@#$%^&*(),.?":{}|<>]/,
+      'Password must contain at least one special character'
+    )
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+    .regex(/[0-9]/, 'Password must contain at least one number');
+}
+
+const signupBodySchema = z
+  .object({
+    firstName: z.string('First name must be a valid string'),
+    lastName: z.string('Last name must be a valid string'),
+    email: z.email('Invalid email address'),
+    password: passwordValidation,
+    passwordConfirm: z.string('Password confirmation must be a valid string'),
+  })
+  .refine(
+    (data) => data.password === data.passwordConfirm,
+    'Passwords do not match'
+  );
+
+type SignupBody = z.infer<typeof signupBodySchema>;
+
+export { signupBodySchema as default, type SignupBody };
