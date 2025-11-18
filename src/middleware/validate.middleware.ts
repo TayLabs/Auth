9c26d66934edfc823ef0f118d3a2d72c3b9bd6d0
Taylor Kelley <taylorgkelley@outlook.com>
2025-11-18 15:57:09 -0600

    fix: zod validation response

--- src/middleware/validate.middleware.ts
+++ src/middleware/validate.middleware.ts
@@ -1,29 +1,31 @@
 import env from '@/types/env';
+import { ResponseBody } from '@/types/ResponseBody';
 import { type RequestHandler } from 'express';
 import z from 'zod';
 
-export function validate<T extends z.ZodObject>(schema: T): RequestHandler {
-  return async (req, res, next) => {
-    try {
-      const validatedData = await schema.parseAsync(req.body);
-      req.body = validatedData;
-    } catch (err) {
-      if (err instanceof z.ZodError) {
-        const errorTree = z.treeifyError<T>(err as z.ZodError<T>).properties;
+export function validate<T extends z.ZodObject>(
+	schema: T
+): RequestHandler<{}, ResponseBody> {
+	return async (req, res, next) => {
+		try {
+			const validatedData = await schema.parseAsync(req.body);
+			req.body = validatedData;
+		} catch (err) {
+			if (err instanceof z.ZodError) {
+				const errorTree = z.treeifyError<T>(err as z.ZodError<T>).properties;
 
-        return res.status(400).json({
-          message: 'Invalid request body',
-          stack:
-            env.NODE_ENV === 'development'
-              ? errorTree &&
-                Object.entries(errorTree).map(
-                  ([key, value]) => `${key}: ${value.errors.join(', ')}`
-                )
-              : undefined,
-        });
-      } else {
-        next(err);
-      }
-    }
-  };
+				return res.status(400).json({
+					success: false,
+					message: errorTree
+						? 'Invalid request body: ' +
+						  Object.entries(errorTree)
+								.map(([key, value]) => `${key}: ${value.errors.join(', ')}`)
+								.join(' | ')
+						: 'Invalid request body',
+				});
+			} else {
+				next(err);
+			}
+		}
+	};
 }
