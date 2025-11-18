355eec4a4e0f4f0d2adbd4da3429ef672317dc1a
Taylor Kelley <taylorgkelley@outlook.com>
2025-11-18 12:03:15 -0600

    feat: Adding redis config

---
+++ src/config/redis/client.ts
@@ -0,0 +1,18 @@
+import Redis from 'ioredis';
+
+const redisClient = new Redis({
+	host: process.env.REDIS_HOST!,
+	port: parseInt(process.env.REDIS_PORT!),
+	maxRetriesPerRequest: 3,
+});
+
+redisClient.on('connect', () => {
+	console.log('📻 Connected to Redis');
+});
+
+redisClient.on('error', (err) => {
+	console.error('🛑 Redis connection error:', err);
+	redisClient.quit(); // Prevents multiple connection attempts
+});
+
+export default redisClient;
