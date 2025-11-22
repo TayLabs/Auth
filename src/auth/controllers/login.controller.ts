3f657f3e0d1a477da8a9cff4e9f4c50c9ebac031
Taylor Kelley <taylorgkelley@outlook.com>
2025-11-22 17:34:10 -0600

    chore: changing docker:dev:* to docker:* scripts

--- src/auth/controllers/login.controller.ts
+++ src/auth/controllers/login.controller.ts
@@ -1,21 +1,43 @@
-import type { LoginReqBody, LoginResBody } from '../dto/login.dto';
 import { controller } from '@/middleware/controller.middleware';
 import HttpStatus from '@/types/HttpStatus.enum';
+import type { LoginReqBody, LoginResBody } from '../dto/login.dto';
+import type { SignupReqBody, SignupResBody } from '../dto/signup.dto';
+import User from '../utils/User.util';
 
 export const loginController = controller<LoginReqBody, LoginResBody>(
-  (req, res, _next) => {
-    // Login logic here
+	(req, res, _next) => {
+		// Login logic here
 
-    res.status(HttpStatus.CREATED).json({
-      success: true,
-      data: {
-        userId: '00000000-0000-0000-0000-000000000000',
-        email: req.body.email,
-        firstName: 'John',
-        lastName: 'Doe',
-        userName:
-          req.body.email.split('@')[0] + Math.floor(Math.random() * 10) * 10,
-      },
-    });
-  }
+		res.status(HttpStatus.OK).json({
+			success: true,
+			data: {
+				userId: '00000000-0000-0000-0000-000000000000',
+				email: req.body.email,
+				firstName: 'John',
+				lastName: 'Doe',
+				userName:
+					req.body.email.split('@')[0] + Math.floor(Math.random() * 10) * 10,
+			},
+		});
+	}
+);
+
+export const signupController = controller<SignupReqBody, SignupResBody>(
+	async (req, res, _next) => {
+		const user = await User.createAsync(req.body.email, req.body.password, {
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+		});
+
+		res.status(HttpStatus.CREATED).json({
+			success: true,
+			data: {
+				userId: user.id,
+				email: user.email,
+				firstName: user.profile.firstName,
+				lastName: user.profile.lastName,
+				username: user.profile.username,
+			},
+		});
+	}
 );
