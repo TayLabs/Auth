4022af14ad47ad95371e5a8b0af1411f193e7386
Taylor Kelley <taylorgkelley@outlook.com>
2025-11-22 17:44:49 -0600

    refactor: rearranging signup route to be under single login controller

--- src/auth/routes/index.routes.ts
+++ src/auth/routes/index.routes.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import { loginRouter } from './login.routes';
-import { signupRouter } from './signup.routes';
 
 const authRoutes = express.Router();
 
 authRoutes.use('/auth', loginRouter);
-authRoutes.use('/auth', signupRouter);
 
 export { authRoutes };
--- src/auth/routes/login.routes.ts
+++ src/auth/routes/login.routes.ts
@@ -1,11 +1,16 @@
 import { validate } from '@/middleware/validate.middleware';
 import express from 'express';
 import loginBodySchema from '../dto/login.dto';
-import { loginController } from '../controllers/login.controller';
+import signupBodySchema from '../dto/signup.dto';
+import {
+	loginController,
+	signupController,
+} from '../controllers/login.controller';
 
 // /auth/*
 const loginRouter = express.Router();
 
 loginRouter.post('/login', validate(loginBodySchema), loginController);
+loginRouter.post('/signup', validate(signupBodySchema), signupController);
 
 export { loginRouter };
