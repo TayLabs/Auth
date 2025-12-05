dfced891131e9414e38be9e5bc26e45a1e1eb917
Taylor Kelley <taylorgkelley04@gmail.com>
2025-12-05 16:35:07 -0600

    fix: registering router for profile

--- src/account/routes/index.routes.ts
+++ src/account/routes/index.routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { totpRouter } from './totp.routes';
 import { securityRouter } from './security.routes';
+import { profileRouter } from './profile.routes';
 
 const accountRoutes = express.Router();
 
 accountRoutes.use('/account/security', securityRouter);
 accountRoutes.use('/account/security/totp', totpRouter);
+accountRoutes.use('/account/profile', profileRouter);
 
 export { accountRoutes };
