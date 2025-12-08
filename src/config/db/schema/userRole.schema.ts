34151639df0d3c207fe3e6bc163f238449498f77
Taylor Kelley <taylorgkelley04@gmail.com>
2025-12-08 15:38:32 -0600

    fix: duplicate user_role relations (resolved)

--- src/config/db/schema/userRole.schema.ts
+++ src/config/db/schema/userRole.schema.ts
@@ -1,23 +1,29 @@
-import { uuid } from 'drizzle-orm/pg-core';
+import { unique, uuid } from 'drizzle-orm/pg-core';
 import { pgTable } from 'drizzle-orm/pg-core';
 import type { UUID } from 'node:crypto';
 import { userTable } from './user.schema';
 import { roleTable } from './role.schema';
 
-export const userRoleTable = pgTable('user_role', {
-	id: uuid('id').$type<UUID>().primaryKey().defaultRandom(),
-	userId: uuid('user_id')
-		.$type<UUID>()
-		.references(() => userTable.id, {
-			onDelete: 'cascade',
-			onUpdate: 'cascade',
-		})
-		.notNull(),
-	roleId: uuid('role_id')
-		.$type<UUID>()
-		.references(() => roleTable.id, {
-			onDelete: 'cascade',
-			onUpdate: 'cascade',
-		})
-		.notNull(),
-});
+export const userRoleTable = pgTable(
+  'user_role',
+  {
+    id: uuid('id').$type<UUID>().primaryKey().defaultRandom(),
+    userId: uuid('user_id')
+      .$type<UUID>()
+      .references(() => userTable.id, {
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      })
+      .notNull(),
+    roleId: uuid('role_id')
+      .$type<UUID>()
+      .references(() => roleTable.id, {
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      })
+      .notNull(),
+  },
+  (table) => [
+    unique('user_role_unique_constraint').on(table.userId, table.roleId),
+  ]
+);
