94661e3a417e032bbd480c57363c8fe7816b6cb2
Taylor Kelley <taylorgkelley04@gmail.com>
2025-12-09 14:20:40 -0600

    fix: fixing delete account routes

--- src/account/controllers/delete.controllers.ts
+++ src/account/controllers/delete.controllers.ts
@@ -1,13 +1,17 @@
 import { controller } from '@/middleware/controller.middleware';
-import {
-  DeleteAccountReqParams,
-  DeleteAccountResBody,
-} from '../dto/deleteAccount.dto';
+import type { DeleteAccountResBody } from '../dto/deleteAccount.dto';
+import HttpStatus from '@/types/HttpStatus.enum';
+import User from '@/services/User.service';
 
-export const deleteAccount = controller<
-  {},
-  DeleteAccountResBody,
-  DeleteAccountReqParams
->(async (req, res, _next) => {
-  // Delete User logic
-});
+export const deleteAccount = controller<{}, DeleteAccountResBody>(
+  async (req, res, _next) => {
+    const user = await new User(req.user.id).delete(); // Delete's all linked records with cascade delete
+
+    res.status(HttpStatus.OK).json({
+      success: true,
+      data: {
+        user,
+      },
+    });
+  }
+);
--- src/account/routes/deleteAccount.routes.ts
+++ src/account/routes/deleteAccount.routes.ts
@@ -1,15 +1,12 @@
 import authenticate from '@/middleware/authenticate.middleware';
-import { validateParams } from '@/middleware/validate.middleware';
 import { Router } from 'express';
-import { deleteAccountParamsSchema } from '../dto/deleteAccount.dto';
 import { deleteAccount } from '../controllers/delete.controllers';
 
 // /account/delete/*
 const deleteAccountRouter = Router({ mergeParams: true });
 
 deleteAccountRouter.delete(
-  '/:userId',
-  validateParams(deleteAccountParamsSchema),
+  '/',
   authenticate({ allow: ['user.write'] }),
   deleteAccount
 );
