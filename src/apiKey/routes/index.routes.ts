ce95680c5f5a4107cb6d683c2c12ed8db2a9340c
Taylor Kelley <taylorgkelley@outlook.com>
2025-12-09 19:26:40 -0600

    migrating api keys to separate service and removing .env.test -_-

--- src/apiKey/routes/index.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import keyRouter from './key.routes';
-
-const apiKeyRouter = Router();
-
-apiKeyRouter.use('/services/:serviceName', keyRouter);
-
-export { apiKeyRouter };
