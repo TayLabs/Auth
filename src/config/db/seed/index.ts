33927d565172f93965e8daa9b77d3c1a996f5deb
Taylor Kelley <taylorgkelley04@gmail.com>
2025-12-18 16:49:21 -0600

    fix: fixing permission in token service

--- src/config/db/seed/index.ts
+++ src/config/db/seed/index.ts
@@ -1,130 +1,130 @@
 import Password from '@/auth/utils/Password.util';
 import { db } from '..';
 import {
-	userTable,
-	profileTable,
-	serviceTable,
-	roleTable,
-	permissionTable,
-	rolePermissionTable,
-	userRoleTable,
+  userTable,
+  profileTable,
+  serviceTable,
+  roleTable,
+  permissionTable,
+  rolePermissionTable,
+  userRoleTable,
 } from '../schema/index.schema';
 import prod from './prod.data';
 import fetchPermissions from '../utils/fetchPermissions';
 import { eq, or } from 'drizzle-orm';
 
 export default async function seed(options?: { includeTestData: boolean }) {
-	try {
-		const config = await fetchPermissions();
+  try {
+    const config = await fetchPermissions();
 
-		await db.transaction(async (tx) => {
-			// insert service, roles, and permissions
-			await tx
-				.insert(serviceTable)
-				.values(
-					config.map((repo) => ({
-						name: repo.service,
-					}))
-				)
-				.onConflictDoNothing();
+    await db.transaction(async (tx) => {
+      // insert service, roles, and permissions
+      await tx
+        .insert(serviceTable)
+        .values(
+          config.map((repo) => ({
+            name: repo.service,
+          }))
+        )
+        .onConflictDoNothing();
 
-			const services = await tx.select().from(serviceTable);
+      const services = await tx.select().from(serviceTable);
 
-			for (const service of services) {
-				const repo = config.find((repo) => repo.service === service.name);
+      for (const service of services) {
+        const repo = config.find((repo) => repo.service === service.name);
 
-				if (repo?.permissions && repo.permissions.length > 0) {
-					await tx
-						.insert(permissionTable)
-						.values(
-							repo?.permissions.map((permission) => ({
-								...permission,
-								serviceId: service.id,
-							}))
-						)
-						.onConflictDoNothing();
-				}
-			}
-		});
+        if (repo?.permissions && repo.permissions.length > 0) {
+          await tx
+            .insert(permissionTable)
+            .values(
+              repo?.permissions.map((permission) => ({
+                ...permission,
+                serviceId: service.id,
+              }))
+            )
+            .onConflictDoNothing();
+        }
+      }
+    });
 
-		await db.transaction(async (tx) => {
-			// insert user/profiles
-			const users = await tx
-				.insert(userTable)
-				.values(
-					await Promise.all(
-						prod.users.map(async (user) => ({
-							...user,
-							passwordHash: await Password.hashAsync(user.passwordHash),
-						}))
-					)
-				)
-				.onConflictDoNothing();
-			await tx.insert(profileTable).values(prod.profiles).onConflictDoNothing();
+    await db.transaction(async (tx) => {
+      // insert user/profiles
+      const users = await tx
+        .insert(userTable)
+        .values(
+          await Promise.all(
+            prod.users.map(async (user) => ({
+              ...user,
+              passwordHash: await Password.hashAsync(user.passwordHash),
+            }))
+          )
+        )
+        .onConflictDoNothing();
+      await tx.insert(profileTable).values(prod.profiles).onConflictDoNothing();
 
-			// insert service, roles, and permissions
-			const services = await tx
-				.select()
-				.from(serviceTable)
-				.where(
-					or(...prod.roles.map((role) => eq(serviceTable.name, role.service)))
-				);
-			await tx
-				.insert(roleTable)
-				.values([
-					...prod.roles.map((role) => ({
-						...role,
-						serviceId: services.find(
-							(service) => service.name === role.service
-						)!.id,
-					})),
-				])
-				.onConflictDoNothing();
+      // insert service, roles, and permissions
+      const services = await tx
+        .select()
+        .from(serviceTable)
+        .where(
+          or(...prod.roles.map((role) => eq(serviceTable.name, role.service)))
+        );
+      await tx
+        .insert(roleTable)
+        .values([
+          ...prod.roles.map((role) => ({
+            ...role,
+            serviceId: services.find(
+              (service) => service.name === role.service
+            )!.id,
+          })),
+        ])
+        .onConflictDoNothing();
 
-			for (const role of prod.roles) {
-				const permissions = await tx
-					.select()
-					.from(permissionTable)
-					.where(
-						or(
-							...role.permissions.map((permission) =>
-								eq(permissionTable.key, permission)
-							)
-						)
-					);
+      for (const role of prod.roles) {
+        const permissions = await tx
+          .select()
+          .from(permissionTable)
+          .where(
+            or(
+              ...role.permissions.map((permission) =>
+                eq(permissionTable.key, permission)
+              )
+            )
+          );
 
-				if (permissions.length > 0) {
-					await tx
-						.insert(rolePermissionTable)
-						.values(
-							permissions.map((permission) => ({
-								permissionId: permission.id,
-								roleId: role.id,
-							}))
-						)
-						.onConflictDoNothing();
-				}
-			}
-			for (const user of prod.users) {
-				const filtered = prod.roles.filter((role) =>
-					user.roles.includes(role.name)
-				);
-				if (filtered.length > 0) {
-					await tx
-						.insert(userRoleTable)
-						.values(
-							filtered.map((role) => ({
-								userId: user.id,
-								roleId: role.id,
-							}))
-						)
-						.onConflictDoNothing();
-				}
-			}
-		});
+        if (permissions.length > 0) {
+          await tx
+            .insert(rolePermissionTable)
+            .values(
+              permissions.map((permission) => ({
+                permissionId: permission.id,
+                roleId: role.id,
+              }))
+            )
+            .onConflictDoNothing();
+        }
+      }
+      for (const user of prod.users) {
+        const filtered = prod.roles.filter((role) =>
+          user.roles.includes(role.name)
+        );
+        if (filtered.length > 0) {
+          await tx
+            .insert(userRoleTable)
+            .values(
+              filtered.map((role) => ({
+                userId: user.id,
+                roleId: role.id,
+              }))
+            )
+            .onConflictDoNothing();
+        }
+      }
+    });
 
-		console.log('🌱 Seed data inserted');
-	} catch (err) {
-		console.error(err);
-	}
+    console.log('🌱 Seed data inserted');
+  } catch (err) {
+    console.error(err);
+  }
 }
--- src/config/db/seed/prod.data.ts
+++ src/config/db/seed/prod.data.ts
@@ -7,55 +7,55 @@ import { serviceTable } from '../schema/service.schema';
 import { permissionTable } from '../schema/permission.schema';
 
 const prod = {
-	users: [
-		{
-			id: '04f83b2f-6468-4474-9002-254cd1ac47f6',
-			email: env.ADMIN.EMAIL,
-			emailVerified: !z.email().safeParse(env.ADMIN.EMAIL).success, // Won't work with sending password reset to email if there's not a valid email, but non-email usernames won't validate properly on that route anyways
-			passwordHash: env.ADMIN.PASSWORD,
-			forcePasswordChange: env.ADMIN.PASSWORD === 'admin', // force change if it's the default password
-			roles: ['admin', 'user'],
-		},
-	] satisfies (typeof userTable.$inferInsert & {
-		roles: string[];
-	})[],
-	profiles: [
-		{
-			userId: '04f83b2f-6468-4474-9002-254cd1ac47f6',
-			firstName: 'TayLab',
-			lastName: 'Admin',
-			displayName: 'Admin',
-			username: 'admin',
-		},
-	] satisfies (typeof profileTable.$inferInsert)[],
-	roles: [
-		{
-			id: '23e28142-85b2-412a-8e20-eeed89bbfa04',
-			name: 'admin',
-			service: 'auth',
-			permissions: [
-				'user.read',
-				'user.write',
-				'service.read',
-				'service.write',
-				'role.read',
-				'role.write',
-				'permission.read',
-				'permission.write',
-				'keys.write',
-			],
-		},
-		{
-			id: 'e5075ee5-2083-4018-bd78-6e266662067d',
-			service: 'auth',
-			name: 'user',
-			permissions: ['user.read', 'user.write'],
-			assignToNewUser: true,
-		},
-	] satisfies (Omit<typeof roleTable.$inferInsert, 'serviceId'> & {
-		service: string;
-		permissions: string[];
-	})[],
+  users: [
+    {
+      id: '04f83b2f-6468-4474-9002-254cd1ac47f6',
+      email: env.ADMIN.EMAIL,
+      emailVerified: !z.email().safeParse(env.ADMIN.EMAIL).success, // Won't work with sending password reset to email if there's not a valid email, but non-email usernames won't validate properly on that route anyways
+      passwordHash: env.ADMIN.PASSWORD,
+      forcePasswordChange: env.ADMIN.PASSWORD === 'admin', // force change if it's the default password
+      roles: ['admin', 'user'],
+    },
+  ] satisfies (typeof userTable.$inferInsert & {
+    roles: string[];
+  })[],
+  profiles: [
+    {
+      userId: '04f83b2f-6468-4474-9002-254cd1ac47f6',
+      firstName: 'TayLab',
+      lastName: 'Admin',
+      displayName: 'Admin',
+      username: 'admin',
+    },
+  ] satisfies (typeof profileTable.$inferInsert)[],
+  roles: [
+    {
+      id: '23e28142-85b2-412a-8e20-eeed89bbfa04',
+      name: 'admin',
+      service: 'auth',
+      permissions: [
+        'user.read',
+        'user.write',
+        'service.read',
+        'service.write',
+        'role.read',
+        'role.write',
+        'permission.read',
+        'permission.write',
+        'key.write',
+      ],
+    },
+    {
+      id: 'e5075ee5-2083-4018-bd78-6e266662067d',
+      service: 'auth',
+      name: 'user',
+      permissions: ['user.read', 'user.write'],
+      assignToNewUser: true,
+    },
+  ] satisfies (Omit<typeof roleTable.$inferInsert, 'serviceId'> & {
+    service: string;
+    permissions: string[];
+  })[],
 };
 
 export default prod;
