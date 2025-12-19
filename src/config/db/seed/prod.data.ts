69edb9c6366c957a21a45f73698dd8350209a884
Taylor Kelley <taylorgkelley@outlook.com>
2025-12-19 16:00:14 -0600

    fix: fixing permission seeding with service names included

--- src/config/db/seed/prod.data.ts
+++ src/config/db/seed/prod.data.ts
@@ -7,55 +7,58 @@ import { serviceTable } from '../schema/service.schema';
 import { permissionTable } from '../schema/permission.schema';
 
 const prod = {
-  users: [
-    {
-      id: '04f83b2f-6468-4474-9002-254cd1ac47f6',
-      email: env.ADMIN.EMAIL,
-      emailVerified: !z.email().safeParse(env.ADMIN.EMAIL).success, // Won't work with sending password reset to email if there's not a valid email, but non-email usernames won't validate properly on that route anyways
-      passwordHash: env.ADMIN.PASSWORD,
-      forcePasswordChange: env.ADMIN.PASSWORD === 'admin', // force change if it's the default password
-      roles: ['admin', 'user'],
-    },
-  ] satisfies (typeof userTable.$inferInsert & {
-    roles: string[];
-  })[],
-  profiles: [
-    {
-      userId: '04f83b2f-6468-4474-9002-254cd1ac47f6',
-      firstName: 'TayLab',
-      lastName: 'Admin',
-      displayName: 'Admin',
-      username: 'admin',
-    },
-  ] satisfies (typeof profileTable.$inferInsert)[],
-  roles: [
-    {
-      id: '23e28142-85b2-412a-8e20-eeed89bbfa04',
-      name: 'admin',
-      service: 'auth',
-      permissions: [
-        'user.read',
-        'user.write',
-        'service.read',
-        'service.write',
-        'role.read',
-        'role.write',
-        'permission.read',
-        'permission.write',
-        'key.write',
-      ],
-    },
-    {
-      id: 'e5075ee5-2083-4018-bd78-6e266662067d',
-      service: 'auth',
-      name: 'user',
-      permissions: ['user.read', 'user.write'],
-      assignToNewUser: true,
-    },
-  ] satisfies (Omit<typeof roleTable.$inferInsert, 'serviceId'> & {
-    service: string;
-    permissions: string[];
-  })[],
+	users: [
+		{
+			id: '04f83b2f-6468-4474-9002-254cd1ac47f6',
+			email: env.ADMIN.EMAIL,
+			emailVerified: !z.email().safeParse(env.ADMIN.EMAIL).success, // Won't work with sending password reset to email if there's not a valid email, but non-email usernames won't validate properly on that route anyways
+			passwordHash: env.ADMIN.PASSWORD,
+			forcePasswordChange: env.ADMIN.PASSWORD === 'admin', // force change if it's the default password
+			roles: ['admin', 'user'],
+		},
+	] satisfies (typeof userTable.$inferInsert & {
+		roles: string[];
+	})[],
+	profiles: [
+		{
+			userId: '04f83b2f-6468-4474-9002-254cd1ac47f6',
+			firstName: 'TayLab',
+			lastName: 'Admin',
+			displayName: 'Admin',
+			username: 'admin',
+		},
+	] satisfies (typeof profileTable.$inferInsert)[],
+	roles: [
+		{
+			id: '23e28142-85b2-412a-8e20-eeed89bbfa04',
+			name: 'admin',
+			service: 'auth',
+			permissions: [
+				'auth:user.read',
+				'auth:user.write',
+				'auth:service.read',
+				'auth:service.write',
+				'auth:role.read',
+				'auth:role.write',
+				'auth:permission.read',
+				'auth:permission.write',
+				'keys:key.write',
+				'keys:key.read',
+				'keys:service.write',
+				'keys:service.read',
+			],
+		},
+		{
+			id: 'e5075ee5-2083-4018-bd78-6e266662067d',
+			service: 'auth',
+			name: 'user',
+			permissions: ['auth:user.read', 'auth:user.write'],
+			assignToNewUser: true,
+		},
+	] satisfies (Omit<typeof roleTable.$inferInsert, 'serviceId'> & {
+		service: string;
+		permissions: string[];
+	})[],
 };
 
 export default prod;
