7fefb523bb0e8f8aefc07c1b05fd8e3bf491c22a
Taylor Kelley <taylorgkelley@outlook.com>
2025-12-20 20:24:18 -0600

    feat: adding proper interfaces for services

--- src/admin/interfaces/permission.interface.ts
+++ src/admin/interfaces/permission.interface.ts
@@ -2,7 +2,6 @@ import type { UUID } from 'node:crypto';
 
 export type Permission = {
 	id: UUID;
-	serviceId: UUID;
 	key: string;
 	description: string | null;
 };
--- src/admin/interfaces/service.interface.ts
+++ src/admin/interfaces/service.interface.ts
@@ -1,6 +1,9 @@
-import { UUID } from 'node:crypto';
+import type { UUID } from 'node:crypto';
+import type { Permission } from './Permission.interface';
 
 export type Service = {
-  id: UUID;
-  name: string;
+	id: UUID;
+	name: string;
+	isExternal: boolean;
+	permissions: Permission[];
 };
