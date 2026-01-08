23c51cc977ad4c74510edefbdb27b77873edeebd
Taylor Kelley <taylorgkelley04@gmail.com>
2026-01-08 10:38:51 -0600

    fix: filtering of permissions to delete

--- src/admin/services/Role.service.ts
+++ src/admin/services/Role.service.ts
@@ -1,8 +1,8 @@
 import { db } from '@/config/db';
 import { roleTable } from '@/config/db/schema/role.schema';
-import { DrizzleQueryError, eq, getTableColumns, or, sql } from 'drizzle-orm';
+import { DrizzleQueryError, eq, getTableColumns, or } from 'drizzle-orm';
 import type { UUID } from 'node:crypto';
-import type { Role as RoleType } from '@/admin/interfaces/Role.interface';
+import type { Role as RoleType } from '@/admin/interfaces/role.interface';
 import HttpStatus from '@/types/HttpStatus.enum';
 import AppError from '@/types/AppError';
 import { rolePermissionTable } from '@/config/db/schema/rolePermission.schema';
@@ -10,225 +10,227 @@ import { permissionTable } from '@/config/db/schema/permission.schema';
 import { DatabaseError } from 'pg';
 
 export default class Role {
-	private _roleId?: UUID;
+  private _roleId?: UUID;
 
-	constructor(roleId?: UUID) {
-		this._roleId = roleId;
-	}
+  constructor(roleId?: UUID) {
+    this._roleId = roleId;
+  }
 
-	public async getAll(): Promise<RoleType[]> {
-		const roles = await db.select().from(roleTable);
+  public async getAll(): Promise<RoleType[]> {
+    const roles = await db.select().from(roleTable);
 
-		if (roles.length < 1) return [];
+    if (roles.length < 1) return [];
 
-		const permissions = await db
-			.select({
-				...getTableColumns(permissionTable),
-				roleId: rolePermissionTable.roleId,
-			})
-			.from(permissionTable)
-			.innerJoin(
-				rolePermissionTable,
-				eq(rolePermissionTable.permissionId, permissionTable.id)
-			)
-			.where(
-				or(...roles.map((role) => eq(rolePermissionTable.roleId, role.id)))
-			);
+    const permissions = await db
+      .select({
+        ...getTableColumns(permissionTable),
+        roleId: rolePermissionTable.roleId,
+      })
+      .from(permissionTable)
+      .innerJoin(
+        rolePermissionTable,
+        eq(rolePermissionTable.permissionId, permissionTable.id)
+      )
+      .where(
+        or(...roles.map((role) => eq(rolePermissionTable.roleId, role.id)))
+      );
 
-		return roles.map((role) => ({
-			...role,
-			permissions: permissions.filter(
-				(permission) => permission.roleId === role.id
-			),
-		}));
-	}
+    return roles.map((role) => ({
+      ...role,
+      permissions: permissions.filter(
+        (permission) => permission.roleId === role.id
+      ),
+    }));
+  }
 
-	public async get(): Promise<RoleType> {
-		if (!this._roleId)
-			throw new AppError('Please specify a role id', HttpStatus.BAD_REQUEST);
+  public async get(): Promise<RoleType> {
+    if (!this._roleId)
+      throw new AppError('Please specify a role id', HttpStatus.BAD_REQUEST);
 
-		const result: RoleType = (
-			await db.select().from(roleTable).where(eq(roleTable.id, this._roleId))
-		)[0] as any;
+    const result: RoleType = (
+      await db.select().from(roleTable).where(eq(roleTable.id, this._roleId))
+    )[0] as any;
 
-		if (!result) {
-			throw new AppError(
-				'A role with that id does not exist',
-				HttpStatus.NOT_FOUND
-			);
-		}
+    if (!result) {
+      throw new AppError(
+        'A role with that id does not exist',
+        HttpStatus.NOT_FOUND
+      );
+    }
 
-		result.permissions = await db
-			.select(getTableColumns(permissionTable))
-			.from(permissionTable)
-			.innerJoin(
-				rolePermissionTable,
-				eq(rolePermissionTable.permissionId, permissionTable.id)
-			)
-			.where(eq(rolePermissionTable.roleId, result.id));
+    result.permissions = await db
+      .select(getTableColumns(permissionTable))
+      .from(permissionTable)
+      .innerJoin(
+        rolePermissionTable,
+        eq(rolePermissionTable.permissionId, permissionTable.id)
+      )
+      .where(eq(rolePermissionTable.roleId, result.id));
 
-		return result;
-	}
+    return result;
+  }
 
-	public async create({
-		name,
-		assignToNewUser,
-		permissions,
-	}: {
-		name: string;
-		assignToNewUser?: boolean;
-		permissions: UUID[];
-	}): Promise<RoleType> {
-		try {
-			let result: RoleType;
-			await db.transaction(async (tx) => {
-				result = (
-					await tx
-						.insert(roleTable)
-						.values({ name, assignToNewUser })
-						.returning()
-				)[0] as any;
+  public async create({
+    name,
+    assignToNewUser,
+    permissions,
+  }: {
+    name: string;
+    assignToNewUser?: boolean;
+    permissions: UUID[];
+  }): Promise<RoleType> {
+    try {
+      let result: RoleType;
+      await db.transaction(async (tx) => {
+        result = (
+          await tx
+            .insert(roleTable)
+            .values({ name, assignToNewUser })
+            .returning()
+        )[0] as any;
 
-				if (permissions.length > 0) {
-					await tx.insert(rolePermissionTable).values(
-						permissions.map((id) => ({
-							permissionId: id,
-							roleId: result.id,
-						}))
-					);
-				}
+        if (permissions.length > 0) {
+          await tx.insert(rolePermissionTable).values(
+            permissions.map((id) => ({
+              permissionId: id,
+              roleId: result.id,
+            }))
+          );
+        }
 
-				// Populate all permissions for role
-				result.permissions = await tx
-					.select(getTableColumns(permissionTable))
-					.from(permissionTable)
-					.innerJoin(
-						rolePermissionTable,
-						eq(rolePermissionTable.roleId, result.id)
-					);
-			});
+        // Populate all permissions for role
+        result.permissions = await tx
+          .select(getTableColumns(permissionTable))
+          .from(permissionTable)
+          .innerJoin(
+            rolePermissionTable,
+            eq(rolePermissionTable.roleId, result.id)
+          );
+      });
 
-			return result!;
-		} catch (err) {
-			if (
-				err instanceof DrizzleQueryError &&
-				err.cause instanceof DatabaseError
-			) {
-				switch (err.cause.code) {
-					case '23505': // unique_violation
-						throw new Error('A role with that name already exist');
-					case '42P01': // undefined_table
-						throw new AppError(
-							'Database table not found',
-							HttpStatus.INTERNAL_SERVER_ERROR
-						);
-					default:
-						throw err;
-				}
-			} else {
-				throw err;
-			}
-		}
-	}
+      return result!;
+    } catch (err) {
+      if (
+        err instanceof DrizzleQueryError &&
+        err.cause instanceof DatabaseError
+      ) {
+        switch (err.cause.code) {
+          case '23505': // unique_violation
+            throw new Error('A role with that name already exist');
+          case '42P01': // undefined_table
+            throw new AppError(
+              'Database table not found',
+              HttpStatus.INTERNAL_SERVER_ERROR
+            );
+          default:
+            throw err;
+        }
+      } else {
+        throw err;
+      }
+    }
+  }
 
-	public async update({
-		name,
-		assignToNewUser,
-		permissions,
-	}: {
-		name?: string;
-		assignToNewUser?: boolean;
-		permissions?: UUID[];
-	}): Promise<RoleType> {
-		try {
-			let result: RoleType;
-			await db.transaction(async (tx) => {
-				result = (
-					await tx
-						.update(roleTable)
-						.set({ name, assignToNewUser })
-						.where(eq(roleTable.id, this._roleId!))
-						.returning()
-				)[0] as any;
+  public async update({
+    name,
+    assignToNewUser,
+    permissions,
+  }: {
+    name?: string;
+    assignToNewUser?: boolean;
+    permissions?: UUID[];
+  }): Promise<RoleType> {
+    try {
+      let result: RoleType;
+      await db.transaction(async (tx) => {
+        result = (
+          await tx
+            .update(roleTable)
+            .set({ name, assignToNewUser })
+            .where(eq(roleTable.id, this._roleId!))
+            .returning()
+        )[0] as any;
 
-				if (permissions && permissions.length > 0) {
-					await tx
-						.insert(rolePermissionTable)
-						.values(
-							permissions.map((permissionId) => ({
-								permissionId,
-								roleId: this._roleId!,
-							}))
-						)
-						.onConflictDoNothing();
-				}
+        if (permissions && permissions.length > 0) {
+          await tx
+            .insert(rolePermissionTable)
+            .values(
+              permissions.map((permissionId) => ({
+                permissionId,
+                roleId: this._roleId!,
+              }))
+            )
+            .onConflictDoNothing();
+        }
 
-				const allPermissions = await tx
-					.select()
-					.from(rolePermissionTable)
-					.where(eq(rolePermissionTable.roleId, this._roleId!));
+        const allPermissions = await tx
+          .select()
+          .from(rolePermissionTable)
+          .where(eq(rolePermissionTable.roleId, this._roleId!));
 
-				const oldPermissions = allPermissions.filter(
-					(existing) =>
-						!permissions?.find((permissionId) => permissionId === existing.id)
-				);
+        const oldPermissions = allPermissions.filter(
+          (existing) =>
+            !permissions?.find(
+              (permissionId) => permissionId === existing.permissionId
+            )
+        );
 
-				if (oldPermissions.length > 0) {
-					await tx
-						.delete(rolePermissionTable)
-						.where(
-							or(
-								...oldPermissions.map((permission) =>
-									eq(rolePermissionTable.permissionId, permission.permissionId)
-								)
-							)
-						);
-				}
+        if (oldPermissions.length > 0) {
+          await tx
+            .delete(rolePermissionTable)
+            .where(
+              or(
+                ...oldPermissions.map((permission) =>
+                  eq(rolePermissionTable.permissionId, permission.permissionId)
+                )
+              )
+            );
+        }
 
-				result.permissions = await tx
-					.select(getTableColumns(permissionTable))
-					.from(permissionTable)
-					.innerJoin(
-						rolePermissionTable,
-						eq(rolePermissionTable.permissionId, permissionTable.id)
-					)
-					.where(eq(rolePermissionTable.roleId, this._roleId!));
-			});
+        result.permissions = await tx
+          .select(getTableColumns(permissionTable))
+          .from(permissionTable)
+          .innerJoin(
+            rolePermissionTable,
+            eq(rolePermissionTable.permissionId, permissionTable.id)
+          )
+          .where(eq(rolePermissionTable.roleId, this._roleId!));
+      });
 
-			return result!;
-		} catch (err) {
-			if (
-				err instanceof DrizzleQueryError &&
-				err.cause instanceof DatabaseError
-			) {
-				switch (err.cause.code) {
-					// case '23505': // unique_violation
-					// 	throw new Error('A role with that name already exist');
-					case '42P01': // undefined_table
-						throw new AppError(
-							'Database table not found',
-							HttpStatus.INTERNAL_SERVER_ERROR
-						);
-					default:
-						throw err;
-				}
-			} else {
-				throw err;
-			}
-		}
-	}
+      return result!;
+    } catch (err) {
+      if (
+        err instanceof DrizzleQueryError &&
+        err.cause instanceof DatabaseError
+      ) {
+        switch (err.cause.code) {
+          // case '23505': // unique_violation
+          // 	throw new Error('A role with that name already exist');
+          case '42P01': // undefined_table
+            throw new AppError(
+              'Database table not found',
+              HttpStatus.INTERNAL_SERVER_ERROR
+            );
+          default:
+            throw err;
+        }
+      } else {
+        throw err;
+      }
+    }
+  }
 
-	public async delete(): Promise<{ id: UUID }> {
-		if (!this._roleId)
-			throw new AppError('Please specify a role id', HttpStatus.BAD_REQUEST);
+  public async delete(): Promise<{ id: UUID }> {
+    if (!this._roleId)
+      throw new AppError('Please specify a role id', HttpStatus.BAD_REQUEST);
 
-		const result = (
-			await db
-				.delete(roleTable)
-				.where(eq(roleTable.id, this._roleId))
-				.returning()
-		)[0];
+    const result = (
+      await db
+        .delete(roleTable)
+        .where(eq(roleTable.id, this._roleId))
+        .returning()
+    )[0];
 
-		return { id: result.id };
-	}
+    return { id: result.id };
+  }
 }
