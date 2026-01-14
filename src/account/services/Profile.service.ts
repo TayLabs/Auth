6b2b34bc383fe1979b6daffd49609bc51a0fc24a
Taylor Kelley <taylorgkelley@outlook.com>
2026-01-13 18:11:02 -0600

    fix: fixing update profile route incase no data was sent and strictly updating certain fields

--- src/account/services/Profile.service.ts
+++ src/account/services/Profile.service.ts
@@ -5,41 +5,53 @@ import type { User } from '@/interfaces/user.interface';
 import { eq, getTableColumns } from 'drizzle-orm';
 import type { UUID } from 'node:crypto';
 import { Profile as ProfileType } from '@/interfaces/profile.interface';
+import AppError from '@/types/AppError';
+import HttpStatus from '@/types/HttpStatus.enum';
 
 export default class Profile {
-  private _userId: UUID;
+	private _userId: UUID;
 
-  constructor(userId: UUID) {
-    this._userId = userId;
-  }
+	constructor(userId: UUID) {
+		this._userId = userId;
+	}
 
-  public async get(): Promise<Omit<User, 'passwordHash'>> {
-    const { passwordHash, ...userColumns } = getTableColumns(userTable);
-    const { userId, ...profileColumns } = getTableColumns(profileTable);
+	public async get(): Promise<Omit<User, 'passwordHash'>> {
+		const { passwordHash, ...userColumns } = getTableColumns(userTable);
+		const { userId, ...profileColumns } = getTableColumns(profileTable);
 
-    const result = await db
-      .select({
-        ...userColumns,
-        profile: profileColumns,
-      })
-      .from(userTable)
-      .innerJoin(profileTable, eq(userTable.id, profileTable.userId))
-      .where(eq(userTable.id, this._userId));
+		const result = await db
+			.select({
+				...userColumns,
+				profile: profileColumns,
+			})
+			.from(userTable)
+			.innerJoin(profileTable, eq(userTable.id, profileTable.userId))
+			.where(eq(userTable.id, this._userId));
 
-    return result[0];
-  }
+		return result[0];
+	}
 
-  public async update(
-    data: Partial<Omit<typeof profileTable.$inferInsert, 'id' | 'userId'>>
-  ): Promise<ProfileType> {
-    const result = (
-      await db
-        .update(profileTable)
-        .set(data)
-        .where(eq(profileTable.userId, this._userId))
-        .returning()
-    )[0];
+	public async update(
+		data: Partial<Omit<typeof profileTable.$inferInsert, 'id' | 'userId'>>
+	): Promise<ProfileType> {
+		if (Object.keys(data).length > 0) {
+			throw new AppError('Nothing has been changed', HttpStatus.BAD_REQUEST);
+		}
 
-    return result;
-  }
+		const result = (
+			await db
+				.update(profileTable)
+				.set({
+					username: data.username,
+					firstName: data.firstName,
+					lastName: data.lastName,
+					displayName: data.displayName,
+					bio: data.bio,
+				})
+				.where(eq(profileTable.userId, this._userId))
+				.returning()
+		)[0];
+
+		return result;
+	}
 }
