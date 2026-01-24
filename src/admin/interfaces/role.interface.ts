35928684ea8cf7cea0a6826ec73f77f108870bdf
Taylor Kelley <taylorgkelley@outlook.com>
2026-01-24 12:12:23 -0600

    fix: updating docker and removing test files in favor of re-initializing them

--- src/admin/interfaces/role.interface.ts
+++ src/admin/interfaces/role.interface.ts
@@ -2,9 +2,9 @@ import type { UUID } from 'node:crypto';
 import type { Permission } from './permission.interface';
 
 export type Role = {
-  id: UUID;
-  name: string;
-  assignToNewUser: boolean;
-  isExternal: boolean;
-  permissions: Permission[];
+	id: UUID;
+	name: string;
+	assignToNewUser: boolean;
+	isExternal: boolean;
+	permissions: Permission[];
 };
