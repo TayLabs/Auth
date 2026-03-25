55aa9b52507a327d9c84e570952e86f02140d302
TaylorGKelley <taylorgkelley04@gmail.com>
2026-03-25 15:53:53 -0500

    fix: seed data error when no permissions

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -1,22 +1,22 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { DatabaseError, Pool } from 'pg';
-import * as schema from './schema/index.schema';
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { DatabaseError, Pool } from "pg";
+import * as schema from "./schema/index.schema";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
 });
-pool.on('connect', () => {
+pool.on("connect", () => {
   // console.log('🔌 Database connection established successfully.');
 });
-pool.on('error', (err) => {
-  if (err instanceof DatabaseError && err.code === '57P01') return; // Ignore 'admin shutdown' errors
+pool.on("error", (err) => {
+  if (err instanceof DatabaseError && err.code === "57P01") return; // Ignore 'admin shutdown' errors
 
-  console.error('🛑 Database connection failed:', err);
+  console.error("🛑 Database connection failed:", err);
   process.exit(1);
 });
 
 const db = drizzle({ client: pool, schema });
-console.log('🔌 Database connection established successfully.');
+console.log("🔌 Database connection established successfully.");
 
 export { db, pool };
